c7ebd0a38e785c827bcb75ba9f24eb8092fa4c0d
Vladyslav Popov <vlad_np@ukr.net>
2025-08-31 10:17:42 +0200

    finish AlergoCod

--- src/view/pages/ManagePageRouter/managePageRouter.tsx
+++ src/view/pages/ManagePageRouter/managePageRouter.tsx
@@ -4,7 +4,10 @@ import { SecondPage } from "../SecondPage/secondPage";
 import { ThirdPage } from "../ThirdPage/thirdPage";
 import { FourthPage } from "../FourthPage/fourthPage";
 import { FifthPage } from "../FifthPage/fifthPage";
-import { SixthPage } from "../SixthPage/SixthPage";
+import { SixthPage } from "../SixthPage/sixthPage";
+import { SeventhPage } from "../SeventhPage/seventhPage";
+import { EightPage } from "../EightPage/eightPage";
+import { NinthPage } from "../NinthPage/ninthPage";
 
 const ManagePageRouter = () => {
   const { pointId, pageId } = useParams();
@@ -18,6 +21,9 @@ const ManagePageRouter = () => {
   if (pageId === "4") return <FourthPage pointId={pointId} />;
   if (pageId === "5") return <FifthPage pointId={pointId} />;
   if (pageId === "6") return <SixthPage pointId={pointId} />;
+  if (pageId === "7") return <SeventhPage pointId={pointId} />;
+  if (pageId === "8") return <EightPage pointId={pointId} />;
+  if (pageId === "9") return <NinthPage pointId={pointId} />;
 
   return <div>Page not found</div>;
 };
