c4de4a236f50d722f8abde55348073658f1050bf
Vladyslav Popov <vlad_np@ukr.net>
2025-09-02 20:36:27 +0200

    finish header

--- src/view/components/Header/index.tsx
+++ src/view/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import s from "./index.module.scss";
-import Logo from "../../../assets/images/logo.jpg";
+import Logo from "../../../assets/images/logo.svg";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Modal } from "../../ui/Modal";
@@ -9,7 +9,6 @@ import {
   useAppDispatch,
   useAppSelector,
 } from "../../../common/helpers/reduxHook";
-// import { QuizThunks } from "../../../store/thunks/quiz.thunks";
 import { QuizActions, QuizState } from "../../../store/reducers/quiz.reducer";
 import FormDataForHospital from "../FormDataForHospital";
 
@@ -17,36 +16,52 @@ export const Header = () => {
   const dispatch = useAppDispatch();
   const location = useLocation();
   const navigate = useNavigate();
-  // const { addQuizAnswerThunk } = useThunks(QuizThunks);
   const { setQuizListAction } = useActions(QuizActions);
   const { quizList } = useAppSelector(QuizState);
 
   const [differentTime, setDifferentTime] = useState<string>("00:00");
-  const [changeText, ] = useState(true);
   // проверка запуска модального окна
   const [isModal, setIsModal] = useState<boolean>(false);
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+  const [pointOfCase, setPointOfCase] = useState("");
+  const [pointOfHospital, setPointOfHospital] = useState("");
 
   useEffect(() => {
     const firstTime = localStorage.getItem("firstTime");
-    if(firstTime) {
+    if (firstTime) {
       const interval = setInterval(() => {
         let currentTime = Number(Date.now());
         let different = currentTime - Number(firstTime);
-        let hours = Math.floor((different) / 3600000);
+        let hours = Math.floor(different / 3600000);
         let minutes = Math.floor(((different % 86400000) % 3600000) / 60000);
         let result =
           (hours > 9 ? hours : `0${hours}`) +
           ":" +
           (minutes > 9 ? minutes : `0${minutes}`);
-        console.log(result)
+        console.log(result);
         setDifferentTime(result);
         return () => clearInterval(interval);
       }, 1000);
       return () => clearInterval(interval);
     }
-  }, [location.pathname, quizList?.firstSymptomsDate, quizList?.firstSymptomsTimeHh, quizList?.firstSymptomsTimeMm]);
- 
+  }, [
+    location.pathname,
+    quizList?.firstSymptomsDate,
+    quizList?.firstSymptomsTimeHh,
+    quizList?.firstSymptomsTimeMm,
+  ]);
+
+  useEffect(() => {
+    if (quizList) {
+      quizList?.pointOfCase
+        ? setPointOfCase(quizList.pointOfCase)
+        : setPointOfCase("");
+      quizList?.pointOfHospital
+        ? setPointOfHospital(quizList.pointOfHospital)
+        : setPointOfHospital("");
+    }
+  }, [quizList]);
+
   const onClickLogoHandler = () => {
     if (location.pathname === "/") return;
     setIsOpenModal(true);
@@ -82,49 +97,46 @@ export const Header = () => {
       {location.pathname === "/" ? (
         <div className={s.titleBox}>
           <h1>
-            Алгоритм оказания помощи <br /> при ИНСУЛЬТЕ В ОСТРОМ ПЕРИОДЕ
+            Оказание помощи <br /> при Неотложном состоянии
           </h1>
         </div>
       ) : (
         <div className={s.timeBox}>
-            {location.pathname !== "/" && <Button  classname={s.homeBtn} onClick={()=>setIsModal(true)}>ГЕОПОЗИЦИЯ</Button>}
-          <div className={s.title}>
-            {changeText === true ? (
-              <span>
-                С момента <br /> начала заполнения чек-листа
-              </span>
-            ) : (
-              <span>
-                С момента <br /> появления симптомов
-              </span>
-            )}
-          </div>
-          <div className={s.time}>
-            <div className={s.hours}>
-              <span>
-                {differentTime.split(":")[0]}
-              </span>
-              <span className={s.unit}>часов</span>
+          {location.pathname !== "/" && (
+            <div className={s.infoCaseWraper}>
+              <div className={s.infoCase}>
+                <p>{pointOfCase}</p>
+                <p>{pointOfHospital}</p>
+              </div>
+              <Button classname={s.homeBtn} onClick={() => setIsModal(true)}>
+                ГЕОПОЗИЦИЯ
+              </Button>
             </div>
-            <span className={s.delimeter}>:</span>
-            <div className={s.minutes}>
-              <span>
-              {differentTime.split(":")[1]}
-              </span>
-              <span className={s.unit}>минут</span>
+          )}
+          <div className={s.title}>
+            <span>Время заполнения:</span>
+            <div className={s.time}>
+              <div className={s.hours}>
+                <span>{differentTime.split(":")[0]}</span>
+                <span className={s.unit}>часов</span>
+              </div>
+              <span className={s.delimeter}>:</span>
+              <div className={s.minutes}>
+                <span>{differentTime.split(":")[1]}</span>
+                <span className={s.unit}>минут</span>
+              </div>
             </div>
           </div>
         </div>
       )}
-        {/* Окно для внесения данных для передачи в больницу */}
-        <Modal
+      {/* Окно для внесения данных для передачи в больницу */}
+      <Modal
         title="Проверьте информацию"
         isVisible={isModal}
         onClose={() => setIsModal(false)}
-        content={
-          <FormDataForHospital/>}
-        />
-        <Modal
+        content={<FormDataForHospital />}
+      />
+      <Modal
         isVisible={isOpenModal}
         onClose={() => setIsOpenModal(false)}
         content={
